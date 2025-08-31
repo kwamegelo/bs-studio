7601786e30413f533c59b1b23723cd0617f6c975
Angelo <kamoateng432@gmail.com>
2025-08-31 00:10:56 +0000

    another commit

---
+++ my-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BookingSystem from "./book.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BookingSystem />
+  </React.StrictMode>
+);
